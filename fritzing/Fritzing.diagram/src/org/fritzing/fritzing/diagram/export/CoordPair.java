fd35bfae21f7016e2d9e957e11ee40ba75b43692
zeveland <zeveland@85f94c43-f63a-0410-9d22-21439860c375>
2007-11-09 10:43:55 +0000

    git-svn-id: http://fritzing.googlecode.com/svn/trunk@42 85f94c43-f63a-0410-9d22-21439860c375

---
+++ fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/export/CoordPair.java
@@ -0,0 +1,16 @@
+package org.fritzing.fritzing.diagram.export;
+
+
+public class CoordPair {
+	public float xVal;
+	public float yVal;
+	
+	public CoordPair() {
+		
+	}
+
+	public CoordPair(float x, float y) {
+		xVal = x;
+		yVal = y;
+	}
+}
