8f4f0c6ca53426533f2051fba39e9c54a75a68f0
zeveland <zeveland@85f94c43-f63a-0410-9d22-21439860c375>
2007-12-26 13:01:18 +0000

    (hopefully) fixes the error where the "Fritzing will now create your board in Eagle as: ...." showed file names with no back-slashes on Windows systems
    Line 175 has my best try at string replacement of single backslashes with double backslashes in the file name argument passed to fritzing_master.ulp when Eagle is launched.
    
    Does this make any sense to anybody?
    
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@251 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/part/FritzingPCBExportAction.java
+++ fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/part/FritzingPCBExportAction.java
@@ -172,7 +172,7 @@ public class FritzingPCBExportAction implements IWorkbenchWindowActionDelegate {
 		}
 		if (Platform.getOS().equals(Platform.OS_WIN32)) {
 			eagleSCH = "\"" + eagleSCH + "\"";
-			eagleBRD = "\"" + eagleBRD + "\"";
+			eagleBRD = "\"" + eagleBRD.replaceAll("\\\\", "\\\\\\\\") + "\"";
 		}
 		// EAGLE parameters
 		String eagleParams = "RUN " 
