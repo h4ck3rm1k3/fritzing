16e7ae95d3ddd9810f42d3cdea833b4c4f7b68b2
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2008-01-16 16:16:38 +0000

    minor changes for access from PartLoaderUpdater
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@397 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/edit/PartLoaderRegistry.java
+++ fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/edit/PartLoaderRegistry.java
@@ -6,6 +6,8 @@ package org.fritzing.fritzing.diagram.edit;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.fritzing.fritzing.diagram.part.FritzingDiagramEditorUtil;
+
 /**
  * Manages PartLoader resources.
  * 
@@ -38,6 +40,11 @@ public class PartLoaderRegistry {
 		super();
 	}
 
+	
+	public PartLoader get(String path) {
+		return this.get(FritzingDiagramEditorUtil.getFritzingLocation(), path, false);
+	}
+
 	/**
 	 * Returns the PartLoader based on the path. If the PartLoader does not exist in the
 	 * cache, creates a new one and caches.
@@ -46,14 +53,14 @@ public class PartLoaderRegistry {
 	 *            a string, either genus.species or the relative path (e.g. libraries/core/arduino/partDescription.xml)
 	 * @return PartLoader
 	 */
-	public PartLoader get(String path) {
+	public PartLoader get(String root, String path, boolean getDocument) {
 		PartLoader value = partLoaderRegistry.get(path);
 		if (value != null) {
 			return value;
 		}
-				
+						
 		value = new PartLoader();
-		boolean result = value.loadXMLFromLibrary(path);
+		boolean result = value.loadXMLFromLibrary(root, path, getDocument);
 		if (result == false) return null;
 		
 		partLoaderRegistry.put(path, value);
