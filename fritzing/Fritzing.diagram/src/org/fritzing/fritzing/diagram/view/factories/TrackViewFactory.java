050154dc888db9957ba72a8f63b186632694a4fa
andre.knoerig <andre.knoerig@85f94c43-f63a-0410-9d22-21439860c375>
2008-01-17 22:07:46 +0000

    don't create a view for Tracks at all -> further breadboard .fzb reduction
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@429 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/view/factories/TrackViewFactory.java
+++ fritzing/Fritzing.diagram/src/org/fritzing/fritzing/diagram/view/factories/TrackViewFactory.java
@@ -7,6 +7,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.gmf.runtime.diagram.core.preferences.PreferencesHint;
 import org.eclipse.gmf.runtime.diagram.ui.view.factories.ConnectionViewFactory;
 import org.eclipse.gmf.runtime.notation.View;
 import org.fritzing.fritzing.diagram.edit.parts.TrackEditPart;
@@ -16,6 +18,25 @@ import org.fritzing.fritzing.diagram.part.FritzingVisualIDRegistry;
  * @generated
  */
 public class TrackViewFactory extends ConnectionViewFactory {
+
+	/*
+	 * Prevent the creation of a view for Tracks completely!
+	 */
+	@Override
+	public View createView(IAdaptable semanticAdapter, View containerView,
+			String semanticHint, int index, boolean persisted,
+			PreferencesHint preferencesHint) {
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.gmf.runtime.diagram.ui.view.factories.AbstractViewFactory#requiresElement(org.eclipse.emf.ecore.EObject, org.eclipse.gmf.runtime.notation.View)
+	 */
+	@Override
+	protected boolean requiresElement(EObject semanticElement, View view) {
+		// TODO Auto-generated method stub
+		return super.requiresElement(semanticElement, view);
+	}
 
 	/**
 	 * @generated NOT
@@ -27,7 +48,7 @@ public class TrackViewFactory extends ConnectionViewFactory {
 		return styles;
 	}
 	
-	/* FIXME: Tried to remove bendpoints here, because we don't need them for tracks.
+	/* Tried to remove bendpoints here, because we don't need them for tracks.
 	 * However, it seems that they are necessary.
 	 */
 //	protected Bendpoints createBendpoints() {
