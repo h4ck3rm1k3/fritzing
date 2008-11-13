a134f12d2b2771bbc329f3b203642c52c5f179ef
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2008-11-13 11:44:48 +0000

    added header text to all source files
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@1486 85f94c43-f63a-0410-9d22-21439860c375

--- phoenix/phoenix/src/main.cpp
+++ phoenix/phoenix/src/main.cpp
@@ -1,19 +1,30 @@
-// -------------------------------------------
-// (c) 2008 Fachhochschule Potsdam
-//
-// This file is open source, part of the Friting project www.fritzing.org.
-// You may use this code as you like
-// as long as you use this header information.
-//
-// FHP is not responsible for any of the contents or what use you make of it.
-//
-//--------------------------------------------
-//
-// $Revision$:
-// $Author$:
-// $Date$
-//
-//--------------------------------------------
+/*******************************************************************
+
+Part of the Fritzing project - http://fritzing.org
+Copyright (c) 2007-08 Fachhochschule Potsdam - http://fh-potsdam.de
+
+Fritzing is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Fritzing is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
+
+********************************************************************
+
+$Revision$:
+$Author$:
+$Date$
+
+********************************************************************/
+
+
 
 #include <QApplication>
 #include <QCoreApplication>
