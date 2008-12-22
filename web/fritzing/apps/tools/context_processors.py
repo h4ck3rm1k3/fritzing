f93a7a83b00d3a3a9680df197c9e222f9974061f
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2008-12-22 16:06:05 +0000

    Changed context processor to use new upstream navigation manager method
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@1955 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/tools/context_processors.py
+++ web/fritzing/apps/tools/context_processors.py
@@ -2,5 +2,5 @@ from pages.models import Page
 
 def pages(request):
     return {
-        'all_pages': Page.objects.root(request.site)
+        'all_pages': Page.objects.navigation(request.site).order_by("tree_id")
     }
