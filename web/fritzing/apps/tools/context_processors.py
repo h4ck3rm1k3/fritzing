d789a74e9672f0ed86786823e7a56c7f23ea152a
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2008-12-23 21:53:29 +0000

    Made context processor return only first and second navigation levels
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2035 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/tools/context_processors.py
+++ web/fritzing/apps/tools/context_processors.py
@@ -1,6 +1,10 @@
 from pages.models import Page
 
 def pages(request):
-    return {
-        'all_pages': Page.objects.navigation(request.site).order_by("tree_id")
-    }
+    """
+    Return all pages that are in the first and second navigation level
+    ordered after the tree_id.
+    """
+    qs = Page.objects.navigation(request.site).filter(
+        level__lte='1').order_by('tree_id')
+    return {'all_pages': qs}
