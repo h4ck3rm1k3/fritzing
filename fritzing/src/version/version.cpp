e01d1c6b1d302edaddb1afbb3ea6c6c40371f518
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2009-02-01 17:29:20 +0000

    version update.
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2300 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/src/version/version.cpp
+++ fritzing/src/version/version.cpp
@@ -31,7 +31,7 @@ $Date$
 											
 QString Version::m_majorVersion("0");
 QString Version::m_minorVersion("1");
-QString Version::m_minorSubVersion("15");
+QString Version::m_minorSubVersion("16");
 QString Version::m_modifier("b");
 QString Version::m_svnRevision("$Revision$:");
 QString Version::m_svnDate("$Date$");
