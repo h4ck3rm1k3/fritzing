958b54ecb7ae5467dee3b7a5b2d22656ffe46d36
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2009-02-12 10:59:05 +0000

    bumping minor sub version to 17
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2383 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/src/version/version.cpp
+++ fritzing/src/version/version.cpp
@@ -31,7 +31,7 @@ $Date$
 									
 QString Version::m_majorVersion("0");
 QString Version::m_minorVersion("1");
-QString Version::m_minorSubVersion("16");
+QString Version::m_minorSubVersion("17");
 QString Version::m_modifier("b");
 QString Version::m_svnRevision("$Revision$:");
 QString Version::m_svnDate("$Date$");
