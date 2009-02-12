91a0091fbd9b5a2462ffd6df0c81b2b4a43088f6
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-02-12 14:56:46 +0000

    Linux run script
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2390 85f94c43-f63a-0410-9d22-21439860c375

---
+++ fritzing/tools/linux/Fritzing.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+appname=`basename $0 | sed s,\.sh$,,`
+dirname=`dirname $0`
+if [ "${dirname:0:1}" != "/" ]; then
+dirname=$PWD/$dirname
+fi
+LD_LIBRARY_PATH="${dirname}/lib"
+export LD_LIBRARY_PATH
+$dirname/$appname $*
+
