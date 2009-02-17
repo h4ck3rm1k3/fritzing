0a662c882ec43bfc569602d0965ad5740eca3610
brendan.howell <brendan.howell@85f94c43-f63a-0410-9d22-21439860c375>
2009-02-17 16:05:26 +0000

    - fixed checking amd64 vs i386
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2409 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/tools/linux_release_script/release.sh
+++ fritzing/tools/linux_release_script/release.sh
@@ -4,36 +4,39 @@ cd compile_folder
 qmake CONFIG+=release -unix
 make
 
-#let's define some variables that we'll need in the future
+#let's define some variables that we'll need to in the future
 arch_aux=`uname -m`
-arch=''
-if arch_aux=='x86_64'
+if [ $arch_aux == 'x86_64' ]
 	then arch='AMD64'
 	else arch='i386'
 fi
 date=`date +%Y.%m.%d`
 release_folder="fritzing.$date.linux.$arch"
 
+echo "making release folder and copying release files"
 mkdir ../$release_folder
 cp -rf bins/ parts/ examples/ Fritzing Fritzing.sh README.txt LICENSE.GPL2 LICENSE.GPL3 ../$release_folder/
 cd ../$release_folder
 chmod +x Fritzing.sh
 
+echo "making library folders"
 mkdir lib
 mkdir lib/imageformats
 mkdir lib/sqldrivers
 mkdir lib/translations
 
 cd lib
+echo "copying libraries"
 cp /usr/lib/libQtCore.so.4 /usr/lib/libQtCore.so.4.4.3 /usr/lib/libQtGui.so.4 /usr/lib/libQtGui.so.4.4.3 /usr/lib/libQtNetwork.so.4 /usr/lib/libQtNetwork.so.4.4.3 /usr/lib/libQtSql.so.4 /usr/lib/libQtSql.so.4.4.3 /usr/lib/libQtSvg.so.4 /usr/lib/libQtSvg.so.4.4.3 /usr/lib/libQtWebKit.so.4 /usr/lib/libQtWebKit.so.4.4.3 /usr/lib/libQtXml.so.4 /usr/lib/libQtXml.so.4.4.3 .
 
 # if is i368 copy the libaudio
-if arch=='i386'
+if [ $arch == 'i386' ]
 	then 
-		echo "coping libaudio libs"
-		cp /usr/lib/libaudio.so /usr/lib/libaudio.so.2 /usr/lib/libaudio.so.2.4 .
+	    cp /usr/lib/libaudio.so /usr/lib/libaudio.so.2 /usr/lib/libaudio.so.2.4 .
+	    echo "copying libaudio files"
 fi
 
+echo "copying image format libs"
 cp /usr/lib/qt4/plugins/imageformats/libqjpeg.so imageformats
 cp /usr/lib/qt4/plugins/sqldrivers/libqsqlite.so sqldrivers
 
