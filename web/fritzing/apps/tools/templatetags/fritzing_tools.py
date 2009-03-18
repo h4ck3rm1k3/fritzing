8a608ac8b116e8de11ae119a1766880312fdcd6c
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-03-18 16:24:30 +0000

    Adds fritzing_tools template tags library
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2655 85f94c43-f63a-0410-9d22-21439860c375

---
+++ web/fritzing/apps/tools/templatetags/fritzing_tools.py
@@ -0,0 +1,34 @@
+from django.db.models import get_model
+from django import template
+from template_utils.nodes import GenericContentNode
+
+class RelatedContentNode(GenericContentNode):
+    """
+    A subclass of GenericContentNode that performs a select on the related
+    entities of the queryset.
+    """
+    def _get_query_set(self):
+        return return self.query_set.select_related()
+
+def do_latest_objects_with_relatives(parser, token):
+    """
+    Retrieves the latest ``num`` objects from a given model, in that
+    model's default ordering, while selecting and stores them in a context variable.
+
+    Syntax::
+
+        {% get_latest_objects_with_relatives [app_name].[model_name] [num] as [varname] %}
+
+    Example::
+
+        {% get_latest_objects_with_relatives comments.freecomment 5 as latest_comments %}
+
+    """
+    bits = token.contents.split()
+    if len(bits) != 5:
+        raise template.TemplateSyntaxError("'%s' tag takes four arguments" % bits[0])
+    if bits [3] != 'as':
+        raise template.TemplateSyntaxError("third argument to '%s' tag must be 'as'" % bits[0])
+    return RelatedContentNode(bits[1], bits[2], bits[4])
+
+register.tag('get_latest_objects_with_relatives', do_latest_objects_with_relatives)
