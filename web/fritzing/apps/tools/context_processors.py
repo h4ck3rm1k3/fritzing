ed0346ec7c71530a8706c66847117e763e9e6d78
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-03-18 16:24:15 +0000

    Don't pass site instance to model manager anymore.
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2654 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/tools/context_processors.py
+++ web/fritzing/apps/tools/context_processors.py
@@ -5,6 +5,6 @@ def pages(request):
     Return all pages that are in the first and second navigation level
     ordered after the tree_id.
     """
-    qs = Page.objects.navigation(request.site).filter(
+    qs = Page.objects.navigation().filter(
         level__lte='1').order_by('tree_id')
     return {'all_pages': qs}
