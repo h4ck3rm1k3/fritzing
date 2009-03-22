8c9abf34edfd88cd12fb0e4e77a64aaa6fb479a5
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-03-22 20:57:16 +0000

    Remove remains of parts_gen
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2693 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/parts_gen/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('fritzing.apps.parts.views.generator',
-    (r'^choose/', 'choose'),
-    (r'^form/', 'form'),
-    (r'^generate/', 'generate'),
-)
