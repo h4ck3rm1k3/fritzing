d15f49eba34525926fb7d98f55de37e55915ca3c
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-03-22 20:54:47 +0000

    Forgetten urls.py from parts_get
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2692 85f94c43-f63a-0410-9d22-21439860c375

---
+++ web/fritzing/apps/parts/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('fritzing.apps.parts.views.generator',
+    (r'^choose/', 'choose'),
+    (r'^form/', 'form'),
+    (r'^generate/', 'generate'),
+)
