0ecd462d3c467a70165efa0127a539ba66ba75bb
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2009-05-13 12:16:54 +0000

    update version to 0.3.0
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@2930 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/src/version/version.cpp
+++ fritzing/src/version/version.cpp
@@ -30,8 +30,8 @@ $Date$
 #include <QStringList>		
 						
 QString Version::m_majorVersion("0");
-QString Version::m_minorVersion("2");
-QString Version::m_minorSubVersion("5");
+QString Version::m_minorVersion("3");
+QString Version::m_minorSubVersion("0");
 QString Version::m_modifier("b");
 QString Version::m_svnRevision("$Revision$:");
 QString Version::m_svnDate("$Date$");
