806b61c827439e242132d942de47dc7d28698cad
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-05-22 23:05:08 +0000

    PEP8 fixes
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3007 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/projects/models.py
+++ web/fritzing/apps/projects/models.py
@@ -12,9 +12,11 @@ from template_utils.markup import formatter
 from licenses.fields import LicenseField
 import mptt
 
+
 class Category(TitleSlugDescriptionModel):
     description_html = models.TextField(editable=False, blank=True, null=True)
-    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    parent = models.ForeignKey('self', null=True, blank=True,
+        related_name='children')
 
     class Meta:
         verbose_name_plural = 'Categories'
@@ -34,19 +36,21 @@ class Category(TitleSlugDescriptionModel):
     def delete(self):
         super(Category, self).delete()
 
+    @models.permalink
     def get_absolute_url(self):
-        return ('project_category_detail', (), { 'slug': self.slug })
-    get_absolute_url = models.permalink(get_absolute_url)
+        return ('project-category-detail', (), {'slug': self.slug})
 
 try:
     mptt.register(Category)
 except mptt.AlreadyRegistered:
     pass
 
+
 class Resource(TimeStampedModel):
     title = models.CharField(_('title'), max_length=255)
     url = models.URLField(verify_exists=False, max_length=255)
-    project = models.ForeignKey('Project', verbose_name=_('project'), related_name='resources')
+    project = models.ForeignKey('Project', verbose_name=_('project'),
+        related_name='resources')
 
     class Meta:
         verbose_name = 'project resource'
@@ -54,6 +58,7 @@ class Resource(TimeStampedModel):
 
     def __unicode__(self):
         return self.url
+
 
 class Project(TitleSlugDescriptionModel, TimeStampedModel):
     KIDS = 1
@@ -72,11 +77,14 @@ class Project(TitleSlugDescriptionModel, TimeStampedModel):
     instructions_html = models.TextField(blank=True, null=True, editable=False)
 
     # users
-    author = models.ForeignKey(User, verbose_name=_('author'), related_name='author')
-    members = models.ManyToManyField(User, verbose_name=_('members'), blank=True, null=True, related_name='member_of')
+    author = models.ForeignKey(User,
+        verbose_name=_('author'), related_name='author')
+    members = models.ManyToManyField(User, verbose_name=_('members'),
+        blank=True, null=True, related_name='member_of')
 
     # flags
-    difficulty = models.IntegerField(_('difficulty'), choices=DIFFICULTIES, blank=True, null=True)
+    difficulty = models.IntegerField(_('difficulty'),
+        choices=DIFFICULTIES, blank=True, null=True)
 
     # categorization
     category = models.ForeignKey(
@@ -89,9 +97,11 @@ class Project(TitleSlugDescriptionModel, TimeStampedModel):
     public = models.BooleanField(_('public'), default=True,
         help_text=_('Is publicly visible on the site.'))
     featured = models.BooleanField(_('featured'), default=False,
-        help_text=_('Featured projects have a special location in the project overview.'))
+        help_text=_('Featured projects have a special location in the '
+                    'project overview.'))
     blessed = models.BooleanField(_('blessed'), default=False,
-        help_text=_('Blessed projects are core projects from the Fritzing developers or other constant contributor.'))
+        help_text=_('Blessed projects are core projects from the Fritzing '
+                    'developers or other constant contributor.'))
 
     objects = models.Manager()
     published = PublicProjectManager()
@@ -110,9 +120,9 @@ class Project(TitleSlugDescriptionModel, TimeStampedModel):
         self.instructions_html = formatter(self.instructions)
         super(Project, self).save(force_insert, force_update)
 
+    @models.permalink
     def get_absolute_url(self):
-        return ('project_detail', (), { 'slug': self.slug })
-    get_absolute_url = models.permalink(get_absolute_url)
+        return ('project-detail', (), {'slug': self.slug})
 
     @property
     def main_images(self):
@@ -129,17 +139,22 @@ class Project(TitleSlugDescriptionModel, TimeStampedModel):
             return diffs.get(self.difficulty, None)
         return None
 
+
 def handle_project_images(instance, filename):
     slug = instance.project.slug
     if len(slug) >= 3:
-        return os.path.join("projects", slug[0], slug[1], slug[2], slug, "images", filename)
+        return os.path.join(
+            "projects", slug[0], slug[1], slug[2], slug, "images", filename)
     return os.path.join("projects", slug, "images", filename)
 
+
 class Image(ImageModel):
-    title = models.CharField(max_length=255, blank=True, null=True, help_text=_('leave empty to populate with filename'))
+    title = models.CharField(max_length=255, blank=True, null=True,
+        help_text=_('leave empty to populate with filename'))
     image = models.ImageField(upload_to=handle_project_images)
     project = models.ForeignKey(Project, related_name='images')
-    user = models.ForeignKey(User, blank=True, null=True, related_name='project_images')
+    user = models.ForeignKey(User, blank=True,
+        null=True, related_name='project_images')
     view_count = models.PositiveIntegerField(editable=False, default=0)
     is_heading = models.BooleanField(default=False)
 
@@ -154,11 +169,14 @@ class Image(ImageModel):
             self.title = self.image.name
         super(Image, self).save(force_insert, force_update)
 
+
 def handle_project_attachment(instance, filename):
     slug = instance.project.slug
     if len(slug) >= 3:
-        return os.path.join("projects", slug[0], slug[1], slug[2], slug, instance.kind, filename)
+        return os.path.join("projects",
+            slug[0], slug[1], slug[2], slug, instance.kind, filename)
     return os.path.join("projects", slug, instance.kind, filename)
+
 
 class Attachment(models.Model):
     FRITZING_TYPE = 'fritzing'
@@ -169,10 +187,13 @@ class Attachment(models.Model):
         (CODE_TYPE, _('code')),
         (EXAMPLE_TYPE, _('example')),
     )
-    title = models.CharField(_('title'), max_length=255, blank=True, null=True, help_text=_('Leave empty to populate with filename'))
+    title = models.CharField(_('title'), max_length=255, blank=True,
+        null=True, help_text=_('Leave empty to populate with filename'))
     attachment = models.FileField(upload_to=handle_project_attachment)
-    project = models.ForeignKey(Project, verbose_name=_('project'), related_name='attachments')
-    user = models.ForeignKey(User, blank=True, null=True, related_name='project_attachments')
+    project = models.ForeignKey(Project,
+        verbose_name=_('project'), related_name='attachments')
+    user = models.ForeignKey(User,
+        blank=True, null=True, related_name='project_attachments')
     kind = models.CharField(max_length=16, choices=ATTACHMENT_TYPES)
 
     class Meta:
