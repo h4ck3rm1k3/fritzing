4c7148fb7698a8fa1723a55273fb7b417dee431d
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-05-26 21:46:21 +0000

    Disable project list and creation for the moment
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3020 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/projects/urls.py
+++ web/fritzing/apps/projects/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from fritzing.apps.projects.views import create, detail, overview
 
 urlpatterns = patterns('',
-    url(r'^$', overview, name='project-overview'),
-    url(r'^create/$', create, name='projects-create'),
+    # url(r'^$', overview, name='project-overview'),
+    # url(r'^create/$', create, name='projects-create'),
     url(r'^detail/(?P<slug>[-\w]+)/$', detail, name='projects-detail'),
 )
