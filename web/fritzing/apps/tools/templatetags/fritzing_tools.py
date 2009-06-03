9bef904fadb75bee5b3a3ef135cb6a4c6e09fb0e
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-06-03 14:55:28 +0000

    Added OrderedContentNode to make it possible to order querysets from the template.
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3072 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/tools/templatetags/fritzing_tools.py
+++ web/fritzing/apps/tools/templatetags/fritzing_tools.py
@@ -14,6 +14,23 @@ class RelatedContentNode(GenericContentNode):
     def _get_query_set(self):
         return self.query_set.select_related()
 
+class OrderedContentNode(RelatedContentNode):
+    """
+    A subclass of GenericContentNode that performs a select on the related
+    entities of the queryset.
+    """
+    def __init__(self, model, num, order_by, varname):
+        if ',' in order_by:
+            order_by = order_by.split(',')
+        if isinstance(order_by, basestring):
+            order_by = [order_by]
+        self.order_by = order_by
+        super(OrderedContentNode, self).__init__(model, num, varname)
+
+    def _get_query_set(self):
+        qs = super(OrderedContentNode, self)._get_query_set()
+        return qs.order_by(*self.order_by)
+
 @register.tag('get_latest_objects_with_relatives')
 def do_latest_objects_with_relatives(parser, token):
     """
@@ -26,14 +43,18 @@ def do_latest_objects_with_relatives(parser, token):
 
     Example::
 
-        {% get_latest_objects_with_relatives comments.freecomment 5 as latest_comments %}
+        {% get_latest_objects_with_relatives comments.freecomment 5 order_by pub_date as latest_comments %}
 
     """
     bits = token.contents.split()
-    if len(bits) != 5:
-        raise template.TemplateSyntaxError("'%s' tag takes four arguments" % bits[0])
-    if bits [3] != 'as':
-        raise template.TemplateSyntaxError("third argument to '%s' tag must be 'as'" % bits[0])
+    if len(bits) not in (5, 7):
+        raise template.TemplateSyntaxError("'%s' tag takes four or six arguments" % bits[0])
+    if not bits[3] in ('as', 'order_by'):
+        raise template.TemplateSyntaxError("third argument to '%s' tag must be 'as' or 'order_by'" % bits[0])
+    if bits[3] == 'order_by':
+        if bits[5] != 'as':
+            raise template.TemplateSyntaxError("fifth argument to '%s' tag must be 'as'" % bits[0])
+        return OrderedContentNode(bits[1], bits[2], bits[4], bits[6])
     return RelatedContentNode(bits[1], bits[2], bits[4])
 
 @register.filter
