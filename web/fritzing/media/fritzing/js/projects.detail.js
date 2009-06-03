cbc814215514fe18c87a79f868a84b4a54bf2124
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-06-03 14:55:06 +0000

    Added colorbox to project detail.
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3071 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/media/fritzing/js/projects.detail.js
+++ web/fritzing/media/fritzing/js/projects.detail.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-    media_path = "/media/fritzing/img/";
-    $('a[rel*=lightbox]').lightBox({
-        imageLoading: media_path + 'lightbox-ico-loading.gif',
-        imageBtnPrev: media_path + 'lightbox-btn-prev.gif',
-        imageBtnNext: media_path + 'lightbox-btn-next.gif',
-        imageBtnClose: media_path + 'lightbox-btn-close.gif',
-        imageBlank: media_path + 'lightbox-blank.gif',
+    $('a[rel*=lightbox]').colorbox({
+        maxHeight: "95%",
+        maxWidth: "95%",
+        preloading: true,
+        sopacity: 1,
     });
 });
