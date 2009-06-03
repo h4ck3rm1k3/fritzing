e85d523122d0382ad05c3311e358562928dace47
leidel <leidel@85f94c43-f63a-0410-9d22-21439860c375>
2009-06-03 19:16:38 +0000

    Moved project overview to /projects/ root URL
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3083 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/projects/urls.py
+++ web/fritzing/apps/projects/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import *
 from fritzing.apps.projects.views import create, detail, overview
 
 urlpatterns = patterns('',
-    url(r'^overview/$', overview, name='projects-overview'),
+    url(r'', overview, name='projects-overview'),
     # url(r'^create/$', create, name='projects-create'),
     url(r'^(?P<slug>[-\w]+)/$', detail, name='projects-detail'),
 )
