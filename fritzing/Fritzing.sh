ac3adf76866f931c5ef7629e2106cef4253b01f9
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-06-12 10:15:50 +0000

    sheel script for startup on linux, updated to use the generic sh command, instead of bash
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3129 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/Fritzing.sh
+++ fritzing/Fritzing.sh
@@ -1,10 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 appname=`basename $0 | sed s,\.sh$,,`
 dirname=`dirname $0`
-if [ "${dirname:0:1}" != "/" ]; then
-dirname=$PWD/$dirname
+if [ "dirname" != "." ]; then
+	dirname=$PWD/$dirname
 fi
 LD_LIBRARY_PATH="${dirname}/lib"
 export LD_LIBRARY_PATH
 $dirname/$appname $*
-
