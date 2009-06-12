43f9da746e66778737398654845cbd526fd2e41d
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-06-12 13:14:18 +0000

    adding a real "where" property to the event entity
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3132 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/events/admin.py
+++ web/fritzing/apps/events/admin.py
@@ -4,6 +4,28 @@ import dregni
 from tinymce.widgets import TinyMCE
 from fritzing.apps.events import models
 
+class EventWhereInLineForm(forms.ModelForm):
+    class Meta:
+        model = models.EventWhere
+    
+    def clean_text(self):
+        raise forms.ValidationError("The field %s cannot be empty" % field_name)
+        return self.clean_aux('text')
+    
+    def clean_url(self):
+        raise forms.ValidationError("The field %s cannot be empty" % field_name)
+        return self.clean_aux('url')
+    
+    def clean_aux(self,field_name):
+        data = self.cleaned_data[field_name]
+        #if(data.strip() == ""):
+        return data
+
+class EventWhereInline(admin.TabularInline):
+    model = models.EventWhere
+    #form = EventWhereInLineForm
+    
+
 class EventForm(forms.ModelForm):
     class Meta:
         model = models.Event
@@ -11,15 +33,17 @@ class EventForm(forms.ModelForm):
     body = forms.CharField(widget=TinyMCE(attrs={'cols': 30, 'rows': 30}))
     description = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'size': 94}))
 
+
 class EventAdmin(dregni.admin.EventAdmin):
     form = EventForm
+    inlines = [EventWhereInline]
     fieldsets = (
         (None, {
             'fields': (('title', 'slug'), 'description', 'body', 'tags'),
         }),
         ('Scheduling', {
             'fields': (('start_date', 'start_time'), ('end_date', 'end_time')),
-        }),
+        })
     )
     search_fields = ('title', 'description', 'body', 'tags')
     
--- web/fritzing/apps/events/models.py
+++ web/fritzing/apps/events/models.py
@@ -1,18 +1,15 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 import dregni.models
-from datetime import date 
+from datetime import date
 
 class Event(dregni.models.Event):
     body = models.TextField(_('body'))
-    _where = None
-    
-    def where(self):
-        if not self._where:
-            for md in self.metadata.all():
-                if md.type.title == "where":
-                    self._where = md
-        return self._where
 
     def has_past(self):
         return self.start_date < date.today()
+
+class EventWhere(models.Model):
+    text = models.CharField(_('text'), max_length=255)
+    url = models.URLField(_('URL'), blank=True, verify_exists=False)
+    event = models.OneToOneField(Event,related_name='where',unique=True,blank=False,null=False)
