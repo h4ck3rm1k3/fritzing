d6db68fb49fae3c7f6ff6247213bd4d06e2ca60b
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-06-16 14:23:01 +0000

    Events updated: - not requiring an url for events location - adding a main link
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3147 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/events/models.py
+++ web/fritzing/apps/events/models.py
@@ -10,11 +10,19 @@ class Event(dregni.models.Event):
         return self.start_date < date.today()
     
 class EventWhere(models.Model):
+    class Meta:
+        verbose_name = 'Where'
+        verbose_name_plural = 'Where'
+        
     text = models.CharField(_('text'), max_length=255)
     url = models.URLField(_('URL'), blank=True, verify_exists=False)
     event = models.OneToOneField(Event,related_name='where',verbose_name=_('Where'),unique=True,blank=False,null=False)
     
 class EventLink(models.Model):
+    class Meta:
+        verbose_name = 'Event main page'
+        verbose_name_plural = 'Event main page'
+        
     text = models.CharField(_('text'), max_length=255)
     url = models.URLField(_('URL'))
     event = models.OneToOneField(Event,related_name='link',verbose_name=_('Event main page'),unique=True,blank=False,null=False)
