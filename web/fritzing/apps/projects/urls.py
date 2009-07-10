079f6047b5e76c38aa6c774e54339f067f429ebd
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-07-10 17:28:01 +0000

    change the create project url to make it harder to find
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3260 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/projects/urls.py
+++ web/fritzing/apps/projects/urls.py
@@ -3,7 +3,7 @@ from fritzing.apps.projects.views import create, detail, overview, edit
 
 urlpatterns = patterns('',
     url(r'^$', overview, name='projects-overview'),
-    url(r'^create/$', create, name='projects-create'),
+    url(r'^hidden-create-project/$', create, name='projects-create'),
     url(r'^edit/(?P<project_id>[-\w]+)/$', edit, name='projects-edit'),
     url(r'^(?P<slug>[-\w]+)/$', detail, name='projects-detail'),
     url(r'^by-user/(?P<username>[-\w]+)/$', overview, name='projects-by-user'),
