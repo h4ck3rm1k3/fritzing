6bdd1f8502c0a84a7e316ffbd4d7cc42435d207b
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-08-13 13:47:17 +0000

    setting the SERVE_MEDIA property to true always (otherwise some javascript files, are not served)
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3336 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/settings.py
+++ web/fritzing/settings.py
@@ -14,7 +14,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 # tells Pinax to serve media through django.views.static.serve.
-SERVE_MEDIA = DEBUG
+SERVE_MEDIA = True
 
 ADMINS = (
     ('Jannis Leidel', 'jannis@leidel.info'),
