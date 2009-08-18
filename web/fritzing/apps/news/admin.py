f5a33388d8e341af322bdd118c6c0cd1d9b5ac40
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-08-18 14:21:36 +0000

    tinymce added to the news section
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3345 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/news/admin.py
+++ web/fritzing/apps/news/admin.py
@@ -1,8 +1,18 @@
 import ticker
 from django.contrib import admin
 from fritzing.apps.news import models
+from django import forms
+from tinymce.widgets import TinyMCE
+
+class NewsEntryForm(forms.ModelForm):
+    class Meta:
+        model = models.NewsEntry
+        
+    content = forms.CharField(widget=TinyMCE(attrs={'cols': 30, 'rows': 30}))
+    content_more = forms.CharField(widget=TinyMCE(attrs={'cols': 30, 'rows': 30}),required=False)
 
 class NewsEntryAdmin(ticker.admin.EntryAdmin):
+    form = NewsEntryForm
     fields = (
         'author',
         'status',
@@ -13,5 +23,7 @@ class NewsEntryAdmin(ticker.admin.EntryAdmin):
         'tags',
         'enable_comments',
     )
+    
+    
     
 admin.site.register(models.NewsEntry, NewsEntryAdmin)
