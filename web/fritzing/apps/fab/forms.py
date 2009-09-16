5d04e4b0432c25286c00541a5718cdaed0442a59
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-09-16 13:43:26 +0000

    trying to add a subform for the order addresses
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3485 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/fab/forms.py
+++ web/fritzing/apps/fab/forms.py
@@ -1,9 +1,11 @@
 from django import forms
-from fritzing.apps.fab.models import FabOrder
+from fritzing.apps.fab.models import FabOrder, FabOrderAddress
 
 
 class FabOrderForm(forms.ModelForm):
-
     class Meta:
         model = FabOrder
 
+class FabOrderAddressForm(forms.ModelForm):
+    class Meta:
+        model = FabOrderAddress
--- web/fritzing/apps/fab/views.py
+++ web/fritzing/apps/fab/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.decorators import login_required
-from fritzing.apps.fab.forms import FabOrderForm
+from fritzing.apps.fab.forms import FabOrderForm, FabOrderAddress
+from fritzing.apps.fab.models import *
 from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.forms.models import inlineformset_factory
+
 
 @login_required
 def create(request, form_class=FabOrderForm):
@@ -11,7 +14,11 @@ def create(request, form_class=FabOrderForm):
         #if project: return HttpResponseRedirect(project.get_absolute_url())
     else:
         form = form_class()
-    #print dir(form['title'])
+    shipping_address_formset = inlineformset_factory(FabOrderAddress, FabOrder, fk_name='shipping_address')
+    billing_address_formset = inlineformset_factory(FabOrderAddress, FabOrder, fk_name='billing_address')
     return render_to_response("fab/fab_form.html", {
+        'shipping_address_formset': shipping_address_formset,
+        'billing_address_formset': billing_address_formset,
         'form': form,
     }, context_instance=RequestContext(request))
+
