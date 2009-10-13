80354f5a921a9952283e43c0509d7b4dc238a1c9
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-13 15:06:46 +0000

    fritzing.sh now should work always, no matter where is it called from (click from a file browser, terminal, relative path even in folders with spaces)
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3554 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/Fritzing.sh
+++ fritzing/Fritzing.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
-appname=`basename $0 | sed s,\.sh$,,`
-dirname=`dirname $0`
-if [ "dirname" != "." ]; then
-	dirname=$PWD/$dirname
+appname="Fritzing"
+dirname=`dirname "${0}"`
+
+if [ "$dirname" = "." ]; then
+	dirname="$PWD/$dirname"
 fi
+
 LD_LIBRARY_PATH="${dirname}/lib"
 export LD_LIBRARY_PATH
+
 "$dirname/$appname" $*
