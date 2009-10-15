5bda4371848658c5df98655dc41b38c26370895f
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-15 15:42:23 +0000

    lots of part generation tweaks
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3562 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/part-gen-scripts/cheetah scripts/dip.bat
+++ fritzing/part-gen-scripts/cheetah scripts/dip.bat
@@ -2,19 +2,16 @@ cd "C:\fritzing2\phoenix\part-gen-scripts\cheetah scripts"
 C:\Python26\python.exe partomatic.py -c .\configs\dip_cfg.cfg -t .\templates\dip.cfg -o .\deleteme\config
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_400mil_fzp.cfg -t .\templates\dip.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_400mil_bread_svg.cfg -t .\templates\dip_bread.svg -o .\deleteme\bread
-C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_400mil_schem_svg.cfg -t .\templates\dip_schem.svg -o .\deleteme\schem
+C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_schem_svg.cfg -t .\templates\dip_schem.svg -o .\deleteme\schem
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_400mil_pcb_svg.cfg -t .\templates\dip_pcb.svg -o .\deleteme\pcb
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_300mil_fzp.cfg -t .\templates\dip.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_300mil_bread_svg.cfg -t .\templates\dip_bread.svg -o .\deleteme\bread
-C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_300mil_schem_svg.cfg -t .\templates\dip_schem.svg -o .\deleteme\schem
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_300mil_pcb_svg.cfg -t .\templates\dip_pcb.svg -o .\deleteme\pcb
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_600mil_fzp.cfg -t .\templates\dip.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_600mil_bread_svg.cfg -t .\templates\dip_bread.svg -o .\deleteme\bread
-C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_600mil_schem_svg.cfg -t .\templates\dip_schem.svg -o .\deleteme\schem
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_600mil_pcb_svg.cfg -t .\templates\dip_pcb.svg -o .\deleteme\pcb
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_900mil_fzp.cfg -t .\templates\dip.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_900mil_bread_svg.cfg -t .\templates\dip_bread.svg -o .\deleteme\bread
-C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_900mil_schem_svg.cfg -t .\templates\dip_schem.svg -o .\deleteme\schem
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\dip_900mil_pcb_svg.cfg -t .\templates\dip_pcb.svg -o .\deleteme\pcb
 
 
--- fritzing/src/partseditor/partseditorview.cpp
+++ fritzing/src/partseditor/partseditorview.cpp
@@ -18,9 +18,9 @@ along with Fritzing.  If not, see <http://www.gnu.org/licenses/>.
 
 ********************************************************************
 
-$Revision: 2676 $:
-$Author: cohen@irascible.com $:
-$Date: 2009-03-21 03:10:39 +0100 (Sat, 21 Mar 2009) $
+$Revision$:
+$Author$:
+$Date$
 
 ********************************************************************/
 
