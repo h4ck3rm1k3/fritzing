3ddcba5cecad2a46940ec2e4a17a5b075749c4c5
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-21 12:42:55 +0000

    added 3.5mm screw terminals
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3583 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/part-gen-scripts/cheetah scripts/pin_header.bat
+++ fritzing/part-gen-scripts/cheetah scripts/pin_header.bat
@@ -4,13 +4,13 @@ C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_female_pin_hea
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_female_pin_header_100mil_bread_svg.cfg -t .\templates\generic-female-header_bread.svg -o .\deleteme\bread
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_female_pin_header_100mil_schem_svg.cfg -t .\templates\generic-female-header_schem.svg -o .\deleteme\schem
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\jumper_100mil_svg.cfg -t .\templates\jumper_pcb.svg -o .\deleteme\pcb
+C:\Python26\python.exe partomatic.py -c .\deleteme\config\jumper_3.5mm_svg.cfg -t .\templates\jumper_3.5mm_pcb.svg -o .\deleteme\pcb
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_rounded_female_pin_header_100mil_fzp.cfg -t .\templates\generic-female-header-rounded.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_rounded_female_pin_header_100mil_bread_svg.cfg -t .\templates\generic-female-header-rounded_bread.svg -o .\deleteme\bread
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_male_pin_header_100mil_fzp.cfg -t .\templates\generic-male-header.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_male_pin_header_100mil_bread_svg.cfg -t .\templates\generic-male-header_bread.svg -o .\deleteme\bread
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\generic_male_pin_header_100mil_schem_svg.cfg -t .\templates\generic-male-header_schem.svg -o .\deleteme\schem
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\screw_terminal_100mil_fzp.cfg -t .\templates\screw_terminal.fzp -o .\deleteme\fzp
+C:\Python26\python.exe partomatic.py -c .\deleteme\config\screw_terminal_3.5mm_fzp.cfg -t .\templates\screw_terminal.fzp -o .\deleteme\fzp
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\screw_terminal_100mil_bread_svg.cfg -t .\templates\screw_terminal_bread.svg -o .\deleteme\bread
 C:\Python26\python.exe partomatic.py -c .\deleteme\config\screw_terminal_100mil_schem_svg.cfg -t .\templates\screw_terminal_schem.svg -o .\deleteme\schem
-
-
