344b3c7e0705479c1a1ee555d218d9851ec2ec88
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-27 12:27:11 +0000

    fabform: confirm dialog for the state change
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3643 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/media/fritzing/js/fabdetails.js
+++ web/fritzing/media/fritzing/js/fabdetails.js
@@ -2,7 +2,18 @@ $(document).ready(function(){
 	var stateSelect = $("#state")
 	if(stateSelect) {
 		stateSelect.change(function(){
-			$("#form").submit()
+			var old_state_text = $("#old_state_label").val()
+			var old_state_val = $("#old_state_value").val()
+			var text =
+"This action will change the state of the order from '"+old_state_text+"' to '"+$("#state :selected").text()+
+"'.\nDo you want to continue?"
+			jConfirm(text, "State Change", function(r){
+				if(r) {
+					$("#form").submit()
+				} else {
+					$('#state option[value='+old_state_val+']').attr("selected","selected"); 
+				}
+			})
 		});
 	}
 })
