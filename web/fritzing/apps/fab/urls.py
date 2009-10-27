adde8956b661f1a1896d28b8f09af0f1bead7170
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-27 17:26:17 +0000

    fabform: - emails - orders listed on the profile page
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3650 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/apps/fab/urls.py
+++ web/fritzing/apps/fab/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls.defaults import patterns, url
 from fritzing.apps.fab.views import create, manufacturer_form, details, state_change
 
 urlpatterns = patterns('',
-    #url(r'^$', overview, name='faborders-overview'),
+    url(r'^$', create),
     url(r'^create/$', create, name='faborder-create'),
     url(r'^manufacturer_form/(?P<manufacturer_id>[-\w]+)/.*$', manufacturer_form, name='manufacturer-form'),
     url(r'^order/state/change/', state_change, name='faborder-state-change'),
