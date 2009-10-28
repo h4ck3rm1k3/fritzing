f7d15a76fc837fb2f285e7e848f6b3cd77e36597
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-28 18:21:04 +0000

    tinyMCE added to the forum posts
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3668 85f94c43-f63a-0410-9d22-21439860c375

---
+++ web/fritzing/media/fritzing/js/forum.tinyMce.js
@@ -0,0 +1,22 @@
+tinyMCE.init({
+	"theme_advanced_toolbar_location": "top",
+	"spellchecker_rpc_url": "/tinymce/spellchecker/",
+	"inline_styles": "true",
+	"language": "en",
+	"spellchecker_languages": "+English=en,Deutsch=de",
+	"apply_source_formatting": "true",
+	"file_browser_callback": "djangoFileBrowser",
+	"theme_advanced_buttons1": "bold,italic,|,indent,outdent,|,bullist,numlist,|,link,unlink,image,pre,|,undo,redo,|,code,",
+	"theme_advanced_resizing": "true",
+	"theme_advanced_buttons3": "",
+	"theme_advanced_statusbar_location": "bottom",
+	"theme": "advanced",
+	"elements": "id_body",
+	"directionality": "ltr",
+	"theme_advanced_resize_horizontal": "true",
+	"plugins": "table,spellchecker,paste,searchreplace",
+	"strict_loading_mode": 1,
+	"theme_advanced_buttons2": "",
+	"dialog_type": "window",
+	"mode": "exact"
+})
