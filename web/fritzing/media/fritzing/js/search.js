7818496e4da25ce698f4e838082858a660456599
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-28 17:01:23 +0000

    forum search
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3661 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/media/fritzing/js/search.js
+++ web/fritzing/media/fritzing/js/search.js
@@ -1,14 +1,20 @@
 String.prototype.trim = function() { return this.replace(/^\s+|\s+$/, ''); };
 
 $(document).ready(function(){
-	$("form.search").submit(function(){
-		var input = $("form.search > input[name=q]");
-		if(input.val().trim()!= "") {
-			input.val("site:fritzing.org "+input.val());
+	googleSearch("search","fritzing.org");
+	googleSearch("forumSearch","fritzing.org/forum");
+})
+
+var googleSearch = function(formClass,siteToSearch) {
+	var formSelector = "form."+formClass 
+	$(formSelector).submit(function(){
+		var input = $(formSelector+" > input[name=q]");
+		if(input.val().trim() != "") {
+			input.val("site:"+siteToSearch+" "+input.val());
 			return true;
 		} else {
 			input.val("")
 			return false;
 		}
 	})
-})
+}
