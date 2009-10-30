5190ab157a33841ae4b40e33fe48b183ba143ee5
merunga <merunga@85f94c43-f63a-0410-9d22-21439860c375>
2009-10-30 13:49:27 +0000

    forum editor, with <code> support
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3675 85f94c43-f63a-0410-9d22-21439860c375

--- web/fritzing/media/fritzing/js/forum.tinyMce.js
+++ web/fritzing/media/fritzing/js/forum.tinyMce.js
@@ -6,7 +6,7 @@ tinyMCE.init({
 	"spellchecker_languages": "+English=en,Deutsch=de",
 	"apply_source_formatting": "true",
 	"file_browser_callback": "djangoFileBrowser",
-	"theme_advanced_buttons1": "bold,italic,|,indent,outdent,|,bullist,numlist,|,link,unlink,image,pre,|,undo,redo,|,code,",
+	"theme_advanced_buttons1": "bold,italic,|,indent,outdent,|,bullist,numlist,|,link,unlink,image,sourcecode,|,undo,redo,|,code,",
 	"theme_advanced_resizing": "true",
 	"theme_advanced_buttons3": "",
 	"theme_advanced_statusbar_location": "bottom",
@@ -18,5 +18,17 @@ tinyMCE.init({
 	"strict_loading_mode": 1,
 	"theme_advanced_buttons2": "",
 	"dialog_type": "window",
-	"mode": "exact"
+	"mode": "exact",
+	setup : function(ed) {
+		// Add a custom button
+		ed.addButton('sourcecode', {
+			title : 'Code',
+			image : '/media/fritzing/img/code-button.png',
+			onclick : function() {
+				ed.focus();
+				var content = ed.selection.getContent();
+				ed.selection.setContent("<code>"+content+"</code>")
+			}
+		});
+	}
 })
