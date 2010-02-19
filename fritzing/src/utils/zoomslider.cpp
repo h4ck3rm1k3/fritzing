2bd258e7b9a0f10d50998dfbeb0185083079173d
andre.knoerig <andre.knoerig@85f94c43-f63a-0410-9d22-21439860c375>
2010-02-19 17:36:48 +0000

    some styling for the new zoomslider
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@3997 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/src/utils/zoomslider.cpp
+++ fritzing/src/utils/zoomslider.cpp
@@ -56,18 +56,20 @@ ZoomSlider::ZoomSlider(QWidget * parent) : QFrame(parent)
 
 	this->setStyleSheet("border:0px; margin:0px; padding:0px;");
 
-	int soFar = 0;
-	QLabel * label = new QLabel(tr("Zoom %:"), this);
-	label->setObjectName("ZoomSliderLabel");
-	label->setGeometry(soFar, 0, 50, HEIGHT);
-	label->setAlignment(Qt::AlignRight);
-	soFar += 50 + 2;
+        int soFar = 0;
 
 	m_lineEdit = new QLineEdit(this);
+        m_lineEdit->setObjectName("ZoomSliderValue");
 	m_lineEdit->setGeometry(soFar, -1, 35, HEIGHT - 1);
 	m_lineEdit->setText(QString("%1").arg(STARTING_VALUE));
 	m_lineEdit->setValidator(new QIntValidator(MIN_VALUE, MAX_VALUE, this));
-	soFar += 35 + 5;
+        soFar += 25 + 5;
+
+        QLabel * label = new QLabel(tr("%"), this);
+        label->setObjectName("ZoomSliderLabel");
+        label->setGeometry(soFar, 0, 20, HEIGHT);
+        label->setAlignment(Qt::AlignLeft);
+        soFar += 20 + 2;
 
 	QPixmap temp(":/resources/images/icons/zoomSliderMinus.png");
 	m_minusButton = new QPushButton(this);
@@ -77,10 +79,12 @@ ZoomSlider::ZoomSlider(QWidget * parent) : QFrame(parent)
 	connect(m_minusButton, SIGNAL(clicked()), this, SLOT(minusClicked()));
 	soFar += temp.width() + 5;
 
-	m_slider = new QSlider(this);
+        m_slider = new QSlider(this);
 	m_slider->setOrientation(Qt::Horizontal);
 	m_slider->setRange(MIN_VALUE, MAX_VALUE);
 	m_slider->setValue(STARTING_VALUE);
+        m_slider->setTickPosition(QSlider::TicksBelow);
+        m_slider->setTickInterval(500);
 	m_slider->setGeometry(soFar, 0, 100, HEIGHT);
 	soFar += 100 + 5;
 
