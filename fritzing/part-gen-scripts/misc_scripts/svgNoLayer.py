cb9ac268e00173e31f9031b6c338c2364850612c
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2010-10-05 06:19:54 +0000

    script for finding svgs with no <g id="[layer]">
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@4505 85f94c43-f63a-0410-9d22-21439860c375

---
+++ fritzing/part-gen-scripts/misc_scripts/svgNoLayer.py
@@ -0,0 +1,66 @@
+# usage:
+#    svgNoLayer.py -d <directory> 
+#    
+#    <directory> is a folder, with subfolders, containing .svg files.  In each svg file in the directory or its children
+#    look for id='[layer]' where layer is the set of all layers in fritzing
+
+import getopt, sys, os, re
+    
+def usage():
+    print """
+usage:
+    svgNoLayer.py -d [directory]
+    
+    <directory> is a folder, with subfolders, containing .svg files.  In each svg file in the directory or its children 
+    look for id='[layer]' where layer is the set of all layers in fritzing
+    """
+    
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hd:", ["help", "directory"])
+    except getopt.GetoptError, err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    outputDir = None
+    
+    for o, a in opts:
+        #print o
+        #print a
+        if o in ("-d", "--directory"):
+            outputDir = a
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit(2)
+        else:
+            assert False, "unhandled option"
+    
+    if(not(outputDir)):
+        usage()
+        sys.exit(2)
+        
+    layers = ["icon","breadboardbreadboard", "breadboard", "breadboardWire", "breadboardLabel", "breadboardNote", "breadboardRuler", "schematic", "schematicWire","schematicTrace","schematicLabel", "schematicRuler", "board", "ratsnest", "silkscreen", "silkscreenLabel", "groundplane", "copper0", "copper0trace", "groundplane1", "copper1", "copper1trace", "silkscreen0", "silkscreen0Label", "soldermask",  "outline",  "keepout", "partimage", "pcbNote", "pcbRuler"]
+ 
+    for root, dirs, files in os.walk(outputDir, topdown=False):
+        for filename in files:
+            if (filename.endswith(".svg")):  
+                infile = open(os.path.join(root, filename), "r")
+                svg = infile.read()
+                infile.close()
+                match = None
+                for layer in layers:
+                    match = re.search('id=[\'\"]' + layer, svg)
+                    if (match != None):
+                        break
+                        
+                if match == None:
+                    print "{0} {1}".format(os.path.join(root, filename), "")
+                else:
+                    print "{1} {0}".format(os.path.join(root, filename), match.group(0))
+
+    
+if __name__ == "__main__":
+    main()
+
