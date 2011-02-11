e9ec7d429b8b332634713355f1a2d99a30b0bc08
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2011-02-11 18:47:32 +0000

    beginnings of a pc deployment script
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@4737 85f94c43-f63a-0410-9d22-21439860c375

---
+++ fritzing/deploy_fritzing.bat
@@ -0,0 +1,11 @@
+cd C:\Users\jonathan\fritzing2
+svn export https://fritzing.googlecode.com/svn/trunk/fritzing/translations ./release/translations
+
+svn export https://fritzing.googlecode.com/svn/trunk/fritzing/bins ./release/bins
+
+svn export https://fritzing.googlecode.com/svn/trunk/fritzing/sketches ./release/sketches
+
+svn export https://fritzing.googlecode.com/svn/trunk/fritzing/parts ./release/parts
+
+del .\release\translations\*.ts
+
