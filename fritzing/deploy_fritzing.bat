be6b2bdbd6e5990492639e1537bc5de4d21d1c9f
cohen@irascible.com <cohen@irascible.com@85f94c43-f63a-0410-9d22-21439860c375>
2012-03-10 14:27:31 +0000

    update deploy bat
    git-svn-id: http://fritzing.googlecode.com/svn/trunk@5910 85f94c43-f63a-0410-9d22-21439860c375

--- fritzing/deploy_fritzing.bat
+++ fritzing/deploy_fritzing.bat
@@ -9,4 +9,8 @@ set svndir="C:\Program Files\SlikSvn\bin\"
 %svndir%svn export https://fritzing.googlecode.com/svn/trunk/fritzing/parts ./release/parts
 
 del .\release\translations\*.ts
-
+cd release
+cd translations
+for /f "usebackq delims=;" %%A in (`dir /b *.qm`) do If %%~zA LSS 1024 del "%%A"
+cd ..
+cd ..
